a6c68972f187e72cf10df1418a56ee378fab0ccc
aggisanjay <aggisanjay123@gmail.com>
2023-03-05 18:15:30 +0000

    first commit

---
+++ importMultipleValues.mjs
@@ -0,0 +1,5 @@
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
+
+console.log(myArray); // Output: ['camel', 265, true, '5.6']
+console.log(bulb); // Output: { type: 'LED', wattage: 9, brand: 'Philips', isOn: false }
+console.log(multiplyByFour(5));
